15b0b94a55bc9024b9e7b4b4746914f2fbd46380
Samuel Ytterbrink <neppord@gmail.com>
2020-12-22 15:39:52 +0100

    Inline factory for sulfuras tests

--- ddd_driven/test_sulfuras.py
+++ ddd_driven/test_sulfuras.py
@@ -1,15 +1,15 @@
-from gilded_rose import GildedRose, create_item
+from gilded_rose import GildedRose, Sulfuras
 
 
 def test_update_sell_in():
-    sulfuras_item = create_item("Sulfuras, Hand of Ragnaros", 2, 10)
+    sulfuras_item = Sulfuras("Sulfuras, Hand of Ragnaros", 2, 10)
     items = [sulfuras_item]
     GildedRose(items).update_quality()
     assert sulfuras_item.sell_in == 2
 
 
 def test_update_quality():
-    sulfuras_item = create_item("Sulfuras, Hand of Ragnaros", 2, 80)
+    sulfuras_item = Sulfuras("Sulfuras, Hand of Ragnaros", 2, 80)
     items = [sulfuras_item]
     GildedRose(items).update_quality()
     assert sulfuras_item.quality == 80
