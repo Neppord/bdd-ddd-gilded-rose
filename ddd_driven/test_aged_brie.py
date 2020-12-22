2ddaf63a32a3a15a071347501d1cdde9680593e2
Samuel Ytterbrink <neppord@gmail.com>
2020-12-22 15:38:25 +0100

    Inline factory for aged brie tests

--- ddd_driven/test_aged_brie.py
+++ ddd_driven/test_aged_brie.py
@@ -1,22 +1,22 @@
-from gilded_rose import GildedRose, create_item
+from gilded_rose import GildedRose, AgedBrie
 
 
 def test_it_decreases_sell_in_by_normal_rate():
-    aged_brie = create_item("Aged Brie", 2, 10)
+    aged_brie = AgedBrie("Aged Brie", 2, 10)
     items = [aged_brie]
     GildedRose(items).update_quality()
     assert aged_brie.sell_in == 1
 
 
 def test_it_increases_in_quality_at_normal_rate():
-    aged_brie = create_item("Aged Brie", 2, 10)
+    aged_brie = AgedBrie("Aged Brie", 2, 10)
     items = [aged_brie]
     GildedRose(items).update_quality()
     assert aged_brie.quality == 11
 
 
 def test_it_increases_in_quality_at_double_rate_when_sell_date_has_passed():
-    aged_brie = create_item("Aged Brie", 0, 10)
+    aged_brie = AgedBrie("Aged Brie", 0, 10)
     items = [aged_brie]
     GildedRose(items).update_quality()
     assert aged_brie.quality == 12
