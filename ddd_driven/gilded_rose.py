5cdfe01804539108f9107b3a026ec2b3f5533090
Samuel Ytterbrink <neppord@gmail.com>
2020-12-22 15:26:39 +0100

    Clean up sulfuras

--- ddd_driven/gilded_rose.py
+++ ddd_driven/gilded_rose.py
@@ -99,33 +99,7 @@ class AgedBrie(RawItem):
 
 class Sulfuras(RawItem):
     def update_item_quality(self):
-        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
-            if self.quality > 0:
-                if self.name != "Sulfuras, Hand of Ragnaros":
-                    self.quality = self.quality - 1
-        else:
-            if self.quality < 50:
-                self.quality = self.quality + 1
-                if self.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if self.sell_in < 11:
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-                    if self.sell_in < 6:
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-        if self.name != "Sulfuras, Hand of Ragnaros":
-            self.sell_in = self.sell_in - 1
-        if self.sell_in < 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if self.quality > 0:
-                        if self.name != "Sulfuras, Hand of Ragnaros":
-                            self.quality = self.quality - 1
-                else:
-                    self.quality = self.quality - self.quality
-            else:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
+        pass
 
 
 class BackstagePass(RawItem):
